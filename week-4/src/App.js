455fab3ad70e8f7404dcf5a34b6373e1dfb7ae7c
Jamie Bergen <jbergen@law.stanford.edu>
2020-04-15 21:45:59 -0700

    Initial set up for simple post reader

--- week-4/src/App.js
+++ week-4/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    posts: [],
+    displayingPostList: true,
+    selectedPostID: 0,
+  };
+
+  apiUrl = `https://ma.tt/wp-json/wp/v2/posts`;
+
+  componentDidMount() {
+    fetch(this.apiUrl)
+      .then((response) => response.json())
+      .then((posts) => {
+        this.setState({ posts: posts });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  postClick = (e) => {
+    const selectedPostID = e.target.parentNode.dataset.id;
+    this.setState({
+      displayingPostList: false,
+      selectedPostID,
+    });
+  };
+
+  returnClick = () => {
+    this.setState({
+      displayingPostList: true,
+      selectedPostID: 0,
+    });
+  };
+
+  getSelectedPost = () => {
+    return this.state.posts.find(
+      ({ id }) => id === parseInt(this.state.selectedPostID)
+    );
+  };
+
+  getSelectedPostContent = () => {
+    return this.getSelectedPost().content.rendered;
+  };
+
+  getSelectedPostTitle = () => {
+    console.log("test");
+    return this.getSelectedPost().title.rendered;
+  };
+
+  render() {
+    return this.state.displayingPostList ? (
+      <>
+        <h1>Posts</h1>
+        {this.state.posts.map((post) => (
+          <li key={post.id} data-id={post.id} className="list-item">
+            <button onClick={this.postClick}>{post.title.rendered}</button>
+          </li>
+        ))}
+      </>
+    ) : (
+      <>
+        <button onClick={this.returnClick}>Back to Post List</button>
+        <h1>{this.getSelectedPostTitle()}</h1>
+        <p>Selected Post ID: {this.state.selectedPostID}</p>
+        {this.getSelectedPostContent()}
+      </>
+    );
+  }
 }
 
 export default App;
