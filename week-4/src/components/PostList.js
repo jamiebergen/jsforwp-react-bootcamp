2ce4af0a9f2a15f087abc8cb2e64240af8a03c42
Jamie Bergen <jbergen@law.stanford.edu>
2020-04-17 16:51:57 -0700

    Pull Posts heading outside of ternary statement to avoid repetition

--- week-4/src/components/PostList.js
+++ week-4/src/components/PostList.js
@@ -13,22 +13,25 @@ class PostList extends React.Component {
 
   render() {
     const { posts, postsLoaded, postClick } = this.props;
-    return postsLoaded ? (
+    return (
       <>
         <h1>Posts</h1>
-        {posts.map((post) => (
-          <li key={post.id} data-id={post.id}>
-            <button
-              onClick={postClick}
-              dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-            ></button>
-          </li>
-        ))}
-      </>
-    ) : (
-      <>
-        <h1>Posts</h1>
-        <p>Loading...</p>
+        {postsLoaded ? (
+          <>
+            {posts.map((post) => (
+              <li key={post.id} data-id={post.id}>
+                <button
+                  onClick={postClick}
+                  dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+                ></button>
+              </li>
+            ))}
+          </>
+        ) : (
+          <>
+            <p>Loading...</p>
+          </>
+        )}
       </>
     );
   }
